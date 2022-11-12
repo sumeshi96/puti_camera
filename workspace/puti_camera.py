300e6be9dc936f8460be672eebeb52e089d91285
sumeshi96 <yumapc96@gmail.com>
2022-11-12 12:38:09 +0900

    外部から呼び出しできるようにした

--- workspace/puti_camera.py
+++ workspace/puti_camera.py
@@ -4,5 +4,6 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 ret,frame = cap.read()
-cv2.imwrite("image.jpg",frame)
+def get_img():
+    cv2.imwrite("image.jpg",frame)
 cap.release
