6c75247ef688842dfac19724a385788033886ca1
sumeshi96 <yumapc96@gmail.com>
2022-11-12 11:44:50 +0900

    構文エラーを解消

--- workspace/puti_camera.py
+++ workspace/puti_camera.py
@@ -1,8 +1,8 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2,CAP_PROP_FRAME_WIDTH,1280)
-cap.set(cv2,CAP_PROP_FRAME_HEIGHT,720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 ret,frame = cap.read()
 cv2.imwrite("image.jpg",frame)
 cap.release
