1aa6729bec3399930c03fa14f6ef1c9b0c553040
sumeshi96 <yumapc96@gmail.com>
2022-11-23 05:00:36 +0000

    画像のフレームをリフレッシュできるようにした

--- workspace/puti_camera.py
+++ workspace/puti_camera.py
@@ -3,7 +3,8 @@ import cv2
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
-ret,frame = cap.read()
 def get_img():
+    ret,frame = cap.read()
+    print(ret)
     cv2.imwrite("image.jpg",frame)
-cap.release
+    cap.release
